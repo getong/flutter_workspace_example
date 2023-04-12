34cecb5fcecbaadd2e5dcfc8e3ccfbc0dab5dc8a
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-12 22:43:43 +0800

    add rive example

---
+++ rive_example/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+void main() => runApp(MaterialApp(
+      home: MyRiveAnimation(),
+    ));
+
+class MyRiveAnimation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: RiveAnimation.network(
+          'https://cdn.rive.app/animations/vehicles.riv',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
