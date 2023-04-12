32e63bb07814df822693976c19b36d2a3741bef8
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-12 10:36:41 +0800

    add flutter bloc example

---
+++ flutter_bloc_example/README.md
@@ -0,0 +1,18 @@
+# flutter_bloc_example
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+copy from [Flutter Bloc Counter Example](https://gist.github.com/felangel/fc8230776591f0297e6a1d1b5ef46a6c)
---
+++ flutter_bloc_example/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(App());
+
+abstract class CounterEvent {}
+
+class Increment extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0) {
+    on<Increment>((event, emit) => emit(state + 1));
+  }
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterBloc(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: Center(
+        child: BlocBuilder<CounterBloc, int>(
+          builder: (context, count) {
+            return Text('$count', style: Theme.of(context).textTheme.headline1);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => context.read<CounterBloc>().add(Increment()),
+      ),
+    );
+  }
+}
