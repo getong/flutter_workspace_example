386d191c57f5d91b37ad78fd79379fda8f8af18b
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-15 23:51:33 +0800

    flutter bloc example

--- flutter_bloc_example/lib/main.dart
+++ flutter_bloc_example/lib/main.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(App());
-
-abstract class CounterEvent {}
-
-class Increment extends CounterEvent {}
-
 class CounterBloc extends Bloc<CounterEvent, int> {
   CounterBloc() : super(0) {
-    on<Increment>((event, emit) => emit(state + 1));
+    on<IncrementEvent>((event, emit) {
+      emit(state + event.value);
+    });
+
+    on<DecrementEvent>((event, emit) {
+      emit(state - event.value);
+    });
+  }
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    if (event is IncrementEvent) {
+      yield state + event.value;
+    } else if (event is DecrementEvent) {
+      yield state - event.value;
+    }
   }
 }
 
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocProvider(
-        create: (_) => CounterBloc(),
-        child: CounterPage(),
-      ),
-    );
+abstract class CounterEvent {}
+
+class IncrementEvent extends CounterEvent {
+  final int value;
+
+  IncrementEvent(this.value);
+
+  IncrementEvent operator +(int other) {
+    return IncrementEvent(this.value + other);
+  }
+}
+
+class DecrementEvent extends CounterEvent {
+  final int value;
+
+  DecrementEvent(this.value);
+
+  DecrementEvent operator -(int other) {
+    return DecrementEvent(this.value - other);
   }
 }
 
@@ -29,17 +48,59 @@ class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter')),
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
       body: Center(
         child: BlocBuilder<CounterBloc, int>(
           builder: (context, count) {
-            return Text('$count', style: Theme.of(context).textTheme.headline1);
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Count: $count',
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () {
+                        context.read<CounterBloc>().add(IncrementEvent(1));
+                      },
+                      child: Icon(Icons.add),
+                    ),
+                    SizedBox(width: 16),
+                    FloatingActionButton(
+                      onPressed: () {
+                        context.read<CounterBloc>().add(DecrementEvent(1));
+                      },
+                      child: Icon(Icons.remove),
+                    ),
+                  ],
+                ),
+              ],
+            );
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => context.read<CounterBloc>().add(Increment()),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Bloc Example',
+      home: BlocProvider(
+        create: (context) => CounterBloc(),
+        child: CounterPage(),
       ),
     );
   }
