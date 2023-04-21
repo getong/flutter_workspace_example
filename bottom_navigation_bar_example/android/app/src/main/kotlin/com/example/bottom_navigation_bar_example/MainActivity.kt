f58eee9fe0ec8f4a9adec48d3272c082f5f2c7f6
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-21 12:29:59 +0800

    add bottom navigation bar example

---
+++ bottom_navigation_bar_example/android/app/src/main/kotlin/com/example/bottom_navigation_bar_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bottom_navigation_bar_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bottom_navigation_bar_example/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+
+  static Widget _buildPage(IconData icon, Color color) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Icon(
+          icon,
+          size: 150,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  final List<Widget> pages = [
+    _buildPage(Icons.home, Colors.pink),
+    _buildPage(Icons.business, Colors.green),
+    _buildPage(Icons.school, Colors.deepPurple),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
