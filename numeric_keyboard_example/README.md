f0d74172d3095c6cda93daa319e0224336028abe
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-04-25 01:11:27 +0800

    add numric_keyboard example

---
+++ numeric_keyboard_example/README.md
@@ -0,0 +1,19 @@
+# numeric_keyboard_example
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+
+copy from [Showing Numeric Input Keyboard in Flutter with Code (2023)](https://www.flutterbeads.com/numeric-input-keyboard-in-flutter/)
---
+++ numeric_keyboard_example/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+// import 'numeric_keyboard/numeric_keyboard_demo.dart';
+import 'package:numeric_keyboard/numeric_keyboard.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NumericKeyboardDemo(),
+    );
+  }
+}
+class NumericKeyboardDemo extends StatelessWidget {
+  const NumericKeyboardDemo({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.jpg',
+                scale: 12,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text(
+                'FlutterBeads',
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+          backgroundColor: const Color(0xff6ae792),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Enter Number'),
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Enter Decimal value'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp('[0-9.,]')),
+              ],
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Enter phone number'),
+              keyboardType: TextInputType.phone,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Enter Number'),
+              keyboardType: defaultTargetPlatform == TargetPlatform.iOS
+              ? TextInputType.numberWithOptions(decimal: true, signed: true)
+              : TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
