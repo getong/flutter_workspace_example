dc3fe0ee448ccd0e286b41ac1652e2847e94c8d6
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-05-05 23:02:58 +0800

    update rive file as asset

--- rive_example/lib/main.dart
+++ rive_example/lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-void main() => runApp(MaterialApp(
-      home: MyRiveAnimation(),
-    ));
+void main() {
+  runApp(MaterialApp(
+    home: MyRiveAnimation(),
+  ));
+}
 
 class MyRiveAnimation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: RiveAnimation.network(
-          'https://cdn.rive.app/animations/vehicles.riv',
-          fit: BoxFit.cover,
+        // child: RiveAnimation.network(
+        //   'https://cdn.rive.app/animations/vehicles.riv',
+        //   fit: BoxFit.cover,
+        // ),
+        child: RiveAnimation.asset(
+          'assets/vehicles.riv',
         ),
       ),
     );
