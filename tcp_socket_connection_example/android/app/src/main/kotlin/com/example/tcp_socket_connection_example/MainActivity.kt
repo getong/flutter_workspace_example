bc0fbeaa5c43f92729ff451f20254fa6060c154c
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-24 17:35:45 +0800

    add tcp_socket_connection example

---
+++ tcp_socket_connection_example/android/app/src/main/kotlin/com/example/tcp_socket_connection_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tcp_socket_connection_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tcp_socket_connection_example/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:tcp_socket_connection/tcp_socket_connection.dart';
+
+// nc -l 10251
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({this.title});
+  final String? title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Instantiating the class with the Ip and the PortNumber
+  TcpSocketConnection socketConnection =
+      TcpSocketConnection(ip: "127.0.0.1", port: 10251);
+
+  String message = "";
+
+  @override
+  void initState() {
+    super.initState();
+    startConnection();
+  }
+
+  //receiving and sending back a custom message
+  void messageReceived(String msg) {
+    setState(() {
+      message = msg;
+    });
+    socketConnection.sendMessage("MessageIsReceived :D ");
+  }
+
+  //starting the connection and listening to the socket asynchronously
+  void startConnection() async {
+    socketConnection.enableConsolePrint(
+        true); //use this to see in the console what's happening
+    if (await socketConnection.canConnect(5000, attempts: 3)) {
+      //check if it's possible to connect to the endpoint
+      await socketConnection.connect(5000, messageReceived, attempts: 3);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("hello world"),
+      ),
+      body: Center(
+        child: Text(
+          'You have received ' + message,
+        ),
+      ),
+    );
+  }
+}
