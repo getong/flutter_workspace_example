96b8b66a1405942624b67d9e7e2287695a103f70
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-29 21:04:40 +0800

    add bottomnavigationbar example

---
+++ bottomnavigationbar_example/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Tabs()
+    );
+  }
+}
+
+
+class Tabs extends StatefulWidget {
+
+  Tabs({Key? key}) : super(key: key);
+
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Demo"),
+      ),
+      body: Text("tabBar"),
+      bottomNavigationBar: BottomNavigationBar(
+
+        currentIndex: 1,
+        onTap: (int index){
+          print(index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "首页"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "分类"
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "设置"
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// copy from http://bbs.itying.com/topic/60e65627f0a2871690598cfc
