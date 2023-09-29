ce22bddcb6597ab70fe5249f71d45da524594995
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-09-29 13:30:58 +0800

    update code

--- go_router_statefulshellbranch_example/lib/main.dart
+++ go_router_statefulshellbranch_example/lib/main.dart
@@ -11,12 +11,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      routerConfig: router,
+      routerConfig: GoRouter(
+        navigatorKey: _rootNavigatorKey,
+        initialLocation: '/feed',
+        routes: <RouteBase>[
+          StatefulShellRoute.indexedStack(
+            builder: (context, state, navigationShell) {
+              // Return the widget that implements the custom shell (e.g a BottomNavigationBar).
+              // The [StatefulNavigationShell] is passed to be able to navigate to other branches in a stateful way.
+              return ScaffoldWithNavbar(navigationShell);
+            },
+            branches: [
+              // The route branch for the 1º Tab
+              StatefulShellBranch(
+                navigatorKey: _sectionNavigatorKey,
+                // Add this branch routes
+                // each routes with its sub routes if available e.g feed/uuid/details
+                routes: <RouteBase>[
+                  GoRoute(
+                    path: '/feed',
+                    builder: (context, state) => const FeedPage(),
+                    routes: <RouteBase>[
+                      GoRoute(
+                        path: 'details',
+                        builder: (context, state) {
+                          return const DetailsPage(label: 'FeedDetails');
+                        },
+                      )
+                    ],
+                  ),
+                ],
+              ),
+
+              // The route branch for 2º Tab
+              StatefulShellBranch(routes: <RouteBase>[
+                // Add this branch routes
+                // each routes with its sub routes if available e.g shope/uuid/details
+                GoRoute(
+                  path: '/shope',
+                  builder: (context, state) {
+                    return const DetailsPage(label: 'Shope');
+                  },
+                ),
+              ])
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -57,54 +104,6 @@ class ScaffoldWithNavbar extends StatelessWidget {
 // Create keys for `root` & `section` navigator avoiding unnecessary rebuilds
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 final _sectionNavigatorKey = GlobalKey<NavigatorState>();
-
-final router = GoRouter(
-  navigatorKey: _rootNavigatorKey,
-  initialLocation: '/feed',
-  routes: <RouteBase>[
-    StatefulShellRoute.indexedStack(
-      builder: (context, state, navigationShell) {
-        // Return the widget that implements the custom shell (e.g a BottomNavigationBar).
-        // The [StatefulNavigationShell] is passed to be able to navigate to other branches in a stateful way.
-        return ScaffoldWithNavbar(navigationShell);
-      },
-      branches: [
-        // The route branch for the 1º Tab
-        StatefulShellBranch(
-          navigatorKey: _sectionNavigatorKey,
-          // Add this branch routes
-          // each routes with its sub routes if available e.g feed/uuid/details
-          routes: <RouteBase>[
-            GoRoute(
-              path: '/feed',
-              builder: (context, state) => const FeedPage(),
-              routes: <RouteBase>[
-                GoRoute(
-                  path: 'details',
-                  builder: (context, state) {
-                    return const DetailsPage(label: 'FeedDetails');
-                  },
-                )
-              ],
-            ),
-          ],
-        ),
-
-        // The route branch for 2º Tab
-        StatefulShellBranch(routes: <RouteBase>[
-          // Add this branch routes
-          // each routes with its sub routes if available e.g shope/uuid/details
-          GoRoute(
-            path: '/shope',
-            builder: (context, state) {
-              return const DetailsPage(label: 'Shope');
-            },
-          ),
-        ])
-      ],
-    ),
-  ],
-);
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key, this.label, this.child});
@@ -147,6 +146,10 @@ class FeedPage extends StatelessWidget {
             ElevatedButton(
               onPressed: () => context.go('/feed/details'),
               child: const Text('Go to feed/details'),
+            ),
+            ElevatedButton(
+              onPressed: () => context.go('/shope'),
+              child: const Text('Go to shope'),
             ),
           ],
         ),
