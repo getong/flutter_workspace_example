049313819d7c9da8664e904536f55b890b78da30
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-02 07:49:32 +0800

    add code

--- flutter_container_widget_example/lib/app-in-page/sign_in_page.dart
+++ flutter_container_widget_example/lib/app-in-page/sign_in_page.dart
@@ -7,8 +7,21 @@ class SignInPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Time Tracker'),
       ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.orange,
+              child: SizedBox(
+                width: 100.0,
+                height: 100.0,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-
 }
