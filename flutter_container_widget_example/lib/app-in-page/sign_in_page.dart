e700dbc03f38dd33ca3afc167d232921f4d5a76e
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-02 08:15:37 +0800

    add ElevatedButton widget

--- flutter_container_widget_example/lib/app-in-page/sign_in_page.dart
+++ flutter_container_widget_example/lib/app-in-page/sign_in_page.dart
@@ -7,21 +7,35 @@ class SignInPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Time Tracker'),
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                width: 100.0,
-                height: 100.0,
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: _buildContent(),
     );
   }
+}
+
+Widget _buildContent() {
+  return Padding(
+    // color: Colors.yellow,
+    padding: EdgeInsets.all(16.0),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Text(
+          'Sign in',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 32.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(height: 8.0),
+        ElevatedButton(
+          child: Text('Sign in with Google'),
+          onPressed: () {
+            print('button pressed');
+          },
+        ),
+      ],
+    ),
+  );
 }
