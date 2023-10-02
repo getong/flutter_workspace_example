b601ffee4b0eb3b30e9eb77279f2335cc41eff5a
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-03 07:36:17 +0800

    add flutter slider widget example 2

---
+++ flutter_slider_widget_example2/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Stateful Widget',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // A widget that will be started on the application startup
+      home: CounterWidget(),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  //initial couter value
+  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stateful Widget'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              color: Colors.blue,
+              child: Text(
+                //displays the current number
+                '$_counter',
+                style: TextStyle(fontSize: 50.0),
+              ),
+            ),
+          ),
+          Slider(
+            min: 0,
+            max: 100,
+            value: _counter.toDouble(),
+            onChanged: (double value) {
+              setState(() {
+                  _counter = value.toInt();
+              });
+            },
+            activeColor: Colors.blue,
+            inactiveColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
