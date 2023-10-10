f400bf39b1099b787869184f2c7c05e1e5b9c451
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-11 07:41:07 +0800

    add riverpod generator example

---
+++ riverpod_generator_example/lib/main.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$provideLabelHash() => r'fd12a618e4685c5b357a359025975f6c2e456a30';
+
+/// See also [provideLabel].
+@ProviderFor(provideLabel)
+final provideLabelProvider = AutoDisposeProvider<String>.internal(
+  provideLabel,
+  name: r'provideLabelProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$provideLabelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ProvideLabelRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
