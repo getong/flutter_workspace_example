a8f9b84dd637cac1082e5d9f64297f317261cdf7
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-13 21:48:54 +0800

    add remove operation code

--- flutter_riverpod_notifier_listview_example/lib/main.dart
+++ flutter_riverpod_notifier_listview_example/lib/main.dart
@@ -16,50 +16,105 @@ class ListNotifier extends Notifier<List<String>> {
   }
 
   void removeItem(int index) {
-    state.removeAt(index);
+    // state.removeAt(index);
+    // state = [...state];
+    state = List.from(state)..removeAt(index);
   }
 }
 
 final listProvider = NotifierProvider<ListNotifier, List<String>>(() {
-    return ListNotifier();
+  return ListNotifier();
 });
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(ProviderScope(
+      child: MaterialApp(
+    title: 'title',
+    home: MyApp(),
+  )));
 }
 
 class MyApp extends ConsumerWidget {
   @override
-  Widget build(BuildContext context, ref) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Riverpod ListView Example'),
-        ),
-        body: Consumer(
-          builder: (context, ref, child) {
-            final list = ref.watch(listProvider);
-            return ListView.builder(
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                return Row(
-                  children: <Widget>[
-                    Text(index.toString()),
-                    Text(list[index]),
-                  ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Riverpod ListView Example'),
+      ),
+      body: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          textBaseline: TextBaseline.alphabetic,
+          children: <Widget>[
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Expanded(
+                      child: FloatingActionButton(
+                    onPressed: () {
+                      final list = ref.watch(listProvider);
+                      if (list.length > 0) {
+                        ref
+                            .read(listProvider.notifier)
+                            .removeItem(list.length - 1);
+                      } else {
+                        _showDialog(context);
+                      }
+                    },
+                    child: Icon(Icons.remove),
+                    backgroundColor: Colors.blue,
+                  )),
+                  Expanded(
+                      child: FloatingActionButton(
+                    onPressed: () {
+                      final newItem =
+                          DateTime.now().toString(); // Example new item
+                      ref.read(listProvider.notifier).addItem(newItem);
+                    },
+                    child: Icon(Icons.add),
+                    backgroundColor: Colors.black,
+                  )),
+                ]),
+            Expanded(
+              child: SizedBox(),
+            ),
+            Expanded(child: Consumer(
+              builder: (context, ref, child) {
+                final list = ref.watch(listProvider);
+                return ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: <Widget>[
+                        Text(index.toString()),
+                        Text(list[index]),
+                      ],
+                    );
+                  },
                 );
               },
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            final newItem = DateTime.now().toString(); // Example new item
-            ref.read(listProvider.notifier).addItem(newItem);
-          },
-          child: Icon(Icons.add),
-        ),
-      ),
+            )),
+          ]),
     );
   }
+}
+
+void _showDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('no element'),
+        content: Text('This is no element, close.'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Close'),
+          ),
+        ],
+      );
+    },
+  );
 }
