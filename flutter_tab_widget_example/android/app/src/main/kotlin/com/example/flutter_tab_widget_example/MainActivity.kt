3dcc61842d23e2097b1c0eb7edf77f40429e96ce
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-13 08:46:46 +0800

    add flutter tab widget example

---
+++ flutter_tab_widget_example/android/app/src/main/kotlin/com/example/flutter_tab_widget_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_tab_widget_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_tab_widget_example/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3, // Number of tabs
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Tab Example'),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Tab 1'),
+                Tab(text: 'Tab 2'),
+                Tab(text: 'Tab 3'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text('Content of Tab 1')),
+              Center(child: Text('Content of Tab 2')),
+              Center(child: Text('Content of Tab 3')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
