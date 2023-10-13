283016dbe45a8575fccfbe90ed42c296c1ad368f
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-13 13:33:58 +0800

    add google_fonts example

---
+++ google_fonts_example/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'example_font_selection.dart';
+import 'example_simple.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.light(useMaterial3: true),
+      home: DefaultTabController(
+        animationDuration: Duration.zero,
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Google Fonts Demo'),
+            bottom: const TabBar(
+              tabs: <Widget>[
+                Tab(text: 'Simple'),
+                Tab(text: 'Select a font'),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: <Widget>[
+              ExampleSimple(),
+              ExampleFontSelection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
