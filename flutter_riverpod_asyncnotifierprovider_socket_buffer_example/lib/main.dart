962d160d3ba7e77dc11acfb1a8934de7b85a7eb3
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-10-26 01:26:41 +0800

    add flutter riverpod asyncnotifierprovider socket buffer example

---
+++ flutter_riverpod_asyncnotifierprovider_socket_buffer_example/lib/main.dart
@@ -0,0 +1,162 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// nc -l 12345
+
+final tcpClientProvider =
+    AsyncNotifierProvider<TcpClientNotifier, TcpClient>(() => TcpClientNotifier());
+
+class TcpClientNotifier extends AsyncNotifier<TcpClient> {
+  late TcpClient _tcpClient;
+
+  @override
+  FutureOr<TcpClient> build() async {
+    // Connect to the TCP server and create a TcpClient object.
+    final tcpClient = await TcpClient.connect(host: '127.0.0.1', port: 12345);
+
+    // Return the TcpClient object.
+    return tcpClient;
+  }
+
+  Future<void> connectToServer(String host, int port) async {
+    state = AsyncLoading();
+    try {
+      await _tcpClient.connectToServer(host, port);
+      state = AsyncData(_tcpClient);
+    } catch (error, stackTrace) {
+      state = AsyncError(error, stackTrace);
+    }
+  }
+
+  Future<void> disconnectFromServer() async {
+    await _tcpClient.disconnectFromServer();
+    state = AsyncData(_tcpClient);
+  }
+
+  Future<void> sendData(String data) async {
+    await _tcpClient.sendData(data);
+  }
+}
+
+class TcpClient {
+  late Socket socket;
+  late StringBuffer buffer = StringBuffer();
+
+  TcpClient(this.socket);
+
+  static Future<TcpClient> connect(
+      {required String host, required int port}) async {
+    var socket = await Socket.connect(host, port);
+    var client = TcpClient(socket);
+    return client;
+  }
+
+  Future<void> connectToServer(String host, int port) async {
+    socket = await Socket.connect(host, port);
+    socket.listen(
+      (List<int> data) {
+        final receivedString = String.fromCharCodes(data);
+        buffer.write(receivedString);
+      },
+      onDone: () {
+        socket.destroy();
+      },
+      onError: (error) {
+        print('Error: $error');
+      },
+    );
+  }
+
+  Future<void> disconnectFromServer() async {
+    await socket.close();
+  }
+
+  Future<void> sendData(String data) async {
+    socket.write(data);
+  }
+}
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  final TextEditingController _dataController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TCP Buffer Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Consumer(builder:
+                  (BuildContext context, WidgetRef ref, Widget? child) {
+                return ElevatedButton(
+                  onPressed: () {
+                    ref
+                        .read(tcpClientProvider.notifier)
+                        .connectToServer('127.0.0.1', 12345);
+                  },
+                  child: Text('Connect to Server'),
+                );
+              }),
+              Consumer(builder:
+                  (BuildContext context, WidgetRef ref, Widget? child) {
+                return ElevatedButton(
+                  onPressed: () {
+                    ref.read(tcpClientProvider.notifier).disconnectFromServer();
+                  },
+                  child: Text('Disconnect from Server'),
+                );
+              }),
+              Consumer(
+                builder: (context, ref, child) {
+                  final bufferData = ref.watch(tcpClientProvider);
+
+                  return bufferData.when(
+                    loading: () => CircularProgressIndicator(),
+                    error: (error, stackTrace) => Text('Error: $error'),
+                    data: (tcpClient) {
+                      final bufferData = tcpClient.buffer.toString();
+                      return Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text('Buffered Data: $bufferData'),
+                      );
+                    },
+                  );
+                },
+              ),
+              TextField(
+                controller: _dataController,
+                decoration: InputDecoration(labelText: 'Enter Data'),
+              ),
+              Consumer(builder: (context, ref, child) {
+                return ElevatedButton(
+                  onPressed: () {
+                    final String data = _dataController.text;
+                    if (data.isNotEmpty) {
+                      ref.read(tcpClientProvider.notifier).sendData(data);
+                      _dataController.clear();
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('Please enter data to send.'),
+                      ));
+                    }
+                  },
+                  child: Text('Send Data'),
+                );
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
