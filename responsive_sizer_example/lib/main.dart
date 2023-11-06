757f3fc215a2fc5df6fd677f365f597940e91873
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-11-06 12:16:09 +0800

    add responsive_sizer example

---
+++ responsive_sizer_example/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          title: 'Responsive Sizer Example',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: Home(),
+        );
+      },
+      maxTabletWidth: 900, // Optional
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    late final String screenTypeText;
+
+    switch (Device.screenType) {
+      case ScreenType.mobile:
+      screenTypeText = "My screen's type is Mobile";
+      break;
+      case ScreenType.tablet:
+      screenTypeText = "My screen's type is Tablet";
+      break;
+      // ScreenType can only be desktop when `maxTabletWidth`
+      // is set in `ResponsiveSizer`
+      case ScreenType.desktop:
+      screenTypeText = "My screen's type is Desktop";
+      break;
+    }
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: Adaptive.h(12.5), // or 12.5.h
+            width: 50.w, // or Adaptive.w(50)
+            color: Colors.black,
+          ),
+          Text(
+            "This text is responsive",
+            style: TextStyle(fontSize: 18.sp), // or Adaptive.sp(12)
+          ),
+          Text(
+            "w=${100.w} h=${100.h}",
+            style: TextStyle(fontSize: 12.sp),
+          ),
+          Device.orientation == Orientation.portrait
+          ? Text("My orientation is Portrait")
+          : Text("My orientation is Landscape"),
+          Text(screenTypeText),
+        ],
+      ),
+    );
+  }
+}
