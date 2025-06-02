18b66fdda19061b33d56874a38a254a726a54d28
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-06-02 15:16:05 +0800

    change to use dio package

--- flutter_web3dart_example/lib/bloc/web3_bloc.dart
+++ flutter_web3dart_example/lib/bloc/web3_bloc.dart
@@ -1,18 +1,52 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:web3dart/web3dart.dart';
 import 'package:wallet/wallet.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'web3_event.dart';
 import 'web3_state.dart';
 
+// Custom Dio Web3 client
+class DioWeb3Client {
+  final Dio _dio;
+  final String _rpcUrl;
+
+  DioWeb3Client(this._rpcUrl)
+      : _dio = Dio(BaseOptions(
+          connectTimeout: const Duration(seconds: 10),
+          receiveTimeout: const Duration(seconds: 10),
+          headers: {'Content-Type': 'application/json'},
+        ));
+
+  Future<int> getNetworkId() async {
+    final response = await _dio.post(_rpcUrl, data: {
+      'jsonrpc': '2.0',
+      'method': 'net_version',
+      'params': [],
+      'id': 1,
+    });
+    return int.parse(response.data['result']);
+  }
+
+  Future<BigInt> getBalance(String address) async {
+    final response = await _dio.post(_rpcUrl, data: {
+      'jsonrpc': '2.0',
+      'method': 'eth_getBalance',
+      'params': [address, 'latest'],
+      'id': 1,
+    });
+    return BigInt.parse(response.data['result'].substring(2), radix: 16);
+  }
+
+  void dispose() {
+    _dio.close();
+  }
+}
+
 class Web3Bloc extends Bloc<Web3Event, Web3State> {
-  late Web3Client _client;
-  late http.Client _httpClient;
+  late DioWeb3Client _client;
 
   Web3Bloc() : super(Web3Initial()) {
-    _httpClient = http.Client();
-    // Initialize client with a reliable public RPC endpoint
-    _client = Web3Client('https://ethereum-rpc.publicnode.com', _httpClient);
+    _client = DioWeb3Client('https://ethereum-rpc.publicnode.com');
     on<ConnectToNetwork>(_onConnectToNetwork);
     on<GetBalance>(_onGetBalance);
   }
@@ -31,11 +65,9 @@ class Web3Bloc extends Bloc<Web3Event, Web3State> {
   void _onGetBalance(GetBalance event, Emitter<Web3State> emit) async {
     emit(Web3Loading());
     try {
-      // Create EthereumAddress from hex string
-      final address = EthereumAddress.fromHex(event.address);
-      final balance = await _client.getBalance(address);
+      final balance = await _client.getBalance(event.address);
       // Convert Wei to Ether manually (1 ETH = 10^18 Wei)
-      final balanceInEther = balance.getInWei / BigInt.from(10).pow(18);
+      final balanceInEther = balance / BigInt.from(10).pow(18);
       emit(Web3BalanceLoaded(balanceInEther.toString(), event.address));
     } catch (e) {
       emit(Web3Error('Failed to get balance: ${e.toString()}'));
@@ -45,7 +77,6 @@ class Web3Bloc extends Bloc<Web3Event, Web3State> {
   @override
   Future<void> close() {
     _client.dispose();
-    _httpClient.close();
     return super.close();
   }
 }
