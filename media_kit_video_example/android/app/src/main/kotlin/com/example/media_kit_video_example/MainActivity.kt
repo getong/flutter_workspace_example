447cfcdbca7d31e3bf0694d1dbffabf1d61c5f8b
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-06-03 15:22:44 +0800

    add media_kit_video example

---
+++ media_kit_video_example/android/app/src/main/kotlin/com/example/media_kit_video_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.media_kit_video_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ media_kit_video_example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <media_kit_libs_windows_video/media_kit_libs_windows_video_plugin_c_api.h>
+#include <media_kit_video/media_kit_video_plugin_c_api.h>
+#include <volume_controller/volume_controller_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  MediaKitLibsWindowsVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitLibsWindowsVideoPluginCApi"));
+  MediaKitVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
+  VolumeControllerPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("VolumeControllerPluginCApi"));
+}
