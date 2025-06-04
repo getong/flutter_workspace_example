baa501e546751da7632157aa5b6cfda198ae10c1
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-06-04 18:09:59 +0800

    add get_it basic example

---
+++ get_it_basic_example/lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:get_it_basic_example/app_model.dart';
+
+// This is our global ServiceLocator
+GetIt getIt = GetIt.instance;
+
+void main() {
+  /// I use signalReady here only to show how to use it. In 99% of the cases
+  /// you don't need it. Just use registerSingletonAsync
+  getIt.registerSingleton<AppModel>(
+    AppModelImplementation(),
+    signalsReady: true,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // Access the instance of the registered AppModel
+    // As we don't know for sure if AppModel is already ready we use isReady
+    getIt.isReady<AppModel>().then(
+      (_) => getIt<AppModel>().addListener(update),
+    );
+    // Alternative
+    // getIt.getAsync<AppModel>().addListener(update);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    getIt<AppModel>().removeListener(update);
+    super.dispose();
+  }
+
+  void update() => setState(() => {});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: FutureBuilder(
+        future: getIt.allReady(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              appBar: AppBar(title: Text(widget.title)),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('You have pushed the button this many times:'),
+                    Text(
+                      getIt<AppModel>().counter.toString(),
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: getIt<AppModel>().incrementCounter,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            );
+          } else {
+            return const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Waiting for initialisation'),
+                SizedBox(height: 16),
+                CircularProgressIndicator(),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
