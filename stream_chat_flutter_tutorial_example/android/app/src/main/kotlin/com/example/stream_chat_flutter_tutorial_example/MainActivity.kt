1a4891edc466f519e3a63a40a48516c769465331
19年梦醒 <3949379+getong@users.noreply.github.com>
2025-06-26 10:56:42 +0800

    add stream chat flutter tutorial example

---
+++ stream_chat_flutter_tutorial_example/android/app/src/main/kotlin/com/example/stream_chat_flutter_tutorial_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stream_chat_flutter_tutorial_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ stream_chat_flutter_tutorial_example/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+void main() async {
+  final client = StreamChatClient('b67pax5b2wdq', logLevel: Level.INFO);
+
+  await client.connectUser(
+    User(id: 'tutorial-flutter'),
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHV0b3JpYWwtZmx1dHRlciJ9.S-MJpoSwDiqyXpUURgO5wVqJ4vKlIVFLSEyrFYCOE1c',
+  );
+
+  runApp(MyApp(client: client));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key, required this.client}) : super(key: key);
+
+  final StreamChatClient client;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: (context, child) => StreamChat(client: client, child: child),
+      home: const ChannelListPage(),
+    );
+  }
+}
+
+class ChannelListPage extends StatefulWidget {
+  const ChannelListPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChannelListPage> createState() => _ChannelListPageState();
+}
+
+class _ChannelListPageState extends State<ChannelListPage> {
+  late final _listController = StreamChannelListController(
+    client: StreamChat.of(context).client,
+    filter: Filter.in_('members', [StreamChat.of(context).currentUser!.id]),
+    channelStateSort: const [SortOption('last_message_at')],
+    limit: 20,
+  );
+
+  @override
+  void dispose() {
+    _listController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamChannelListView(
+        controller: _listController,
+        onChannelTap: (channel) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return StreamChannel(
+                  channel: channel,
+                  child: const ChannelPage(),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ChannelPage extends StatelessWidget {
+  const ChannelPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const StreamChannelHeader(),
+      body: Column(
+        children: const <Widget>[
+          Expanded(child: StreamMessageListView()),
+          StreamMessageInput(),
+        ],
+      ),
+    );
+  }
+}
+
+// copy from https://getstream.io/chat/flutter/tutorial/
