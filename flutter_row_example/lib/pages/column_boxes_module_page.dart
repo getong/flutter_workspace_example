f02b8d99646deccc0f74210074abb04375951ab5
gerald <3949379+getong@users.noreply.github.com>
2026-03-29 00:00:15 +0800

    add GestureDetector

--- flutter_row_example/lib/pages/column_boxes_module_page.dart
+++ flutter_row_example/lib/pages/column_boxes_module_page.dart
@@ -4,7 +4,7 @@ import 'package:go_router/go_router.dart';
 class ColumnBoxesModulePage extends StatelessWidget {
   const ColumnBoxesModulePage({super.key});
 
-  List<Widget> boxes(int n, double w, double h) {
+  List<Widget> boxes(BuildContext context, int n, double w, double h) {
     final List<Widget> bxs = <Widget>[];
     const List<Color> fill = <Color>[
       Colors.blue,
@@ -14,12 +14,26 @@ class ColumnBoxesModulePage extends StatelessWidget {
     ];
 
     for (int i = 0; i <= n - 1; i++) {
-      final Container bx = Container(
-        alignment: Alignment.center,
-        color: fill[i % fill.length],
-        width: w,
-        height: h,
-        child: Text(i.toString()),
+      final Widget bx = MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: GestureDetector(
+          onTap: () {
+            final String value = i.toString();
+            debugPrint(value);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                  content: Text(value),
+                  duration: const Duration(milliseconds: 600)),
+            );
+          },
+          child: Container(
+            alignment: Alignment.center,
+            color: fill[i % fill.length],
+            width: w,
+            height: h,
+            child: Text(i.toString()),
+          ),
+        ),
       );
       bxs.add(bx);
     }
@@ -45,7 +59,7 @@ class ColumnBoxesModulePage extends StatelessWidget {
           padding: const EdgeInsets.all(16),
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            children: boxes(4, 40, 40),
+            children: boxes(context, 4, 40, 40),
           ),
         ),
       ),
