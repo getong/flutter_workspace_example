c2f37eef95e225266427841607d73fb9044c7783
gerald <3949379+getong@users.noreply.github.com>
2026-04-01 04:00:18 +0800

    modify home page

--- widget_layout_example/lib/app_router.dart
+++ widget_layout_example/lib/app_router.dart
@@ -3,7 +3,7 @@ import 'package:go_router/go_router.dart';
 
 import 'package:widget_layout_example/modules/center_box_page.dart';
 import 'package:widget_layout_example/modules/constrained_box_page.dart';
-import 'package:widget_layout_example/pages/home_page.dart';
+import 'package:widget_layout_example/home_page.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
