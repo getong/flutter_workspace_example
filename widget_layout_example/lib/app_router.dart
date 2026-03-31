dfedc7c314ef6e2ea2ae8b2dff5282776a4f9f0a
gerald <3949379+getong@users.noreply.github.com>
2026-04-01 03:54:28 +0800

    add app_router.dart

---
+++ widget_layout_example/lib/app_router.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:widget_layout_example/modules/center_box_page.dart';
+import 'package:widget_layout_example/modules/constrained_box_page.dart';
+import 'package:widget_layout_example/pages/home_page.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomePage();
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'center-box',
+            builder: (BuildContext context, GoRouterState state) {
+              return const CenterBoxPage();
+            },
+          ),
+          GoRoute(
+            path: 'constrained-box',
+            builder: (BuildContext context, GoRouterState state) {
+              return const ConstrainedBoxPage();
+            },
+          ),
+        ],
+      ),
+    ],
+  );
+
+  const AppRouter._();
+}
--- widget_layout_example/lib/main.dart
+++ widget_layout_example/lib/main.dart
@@ -1,38 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import 'package:widget_layout_example/modules/center_box_page.dart';
-import 'package:widget_layout_example/modules/constrained_box_page.dart';
-import 'package:widget_layout_example/pages/home_page.dart';
+import 'package:widget_layout_example/app_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomePage();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'center-box',
-          builder: (BuildContext context, GoRouterState state) {
-            return const CenterBoxPage();
-          },
-        ),
-        GoRoute(
-          path: 'constrained-box',
-          builder: (BuildContext context, GoRouterState state) {
-            return const ConstrainedBoxPage();
-          },
-        ),
-      ],
-    ),
-  ],
-);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -44,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      routerConfig: _router,
+      routerConfig: AppRouter.router,
     );
   }
 }
